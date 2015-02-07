ef9e6a50e3a58ae79b7d8e74997feec16fd8bec9
fifth <fifth0725@gmail.com>
2015-02-07 12:04:39 +0000

    2015.2.7 add:default timezone

--- tokyo-time.php
+++ tokyo-time.php
@@ -1,5 +1,9 @@
 <?php
-	$timezone=$_GET['timezone'];
+	if ($_REQUEST['timezone']) {
+		$timezone=$_REQUEST['timezone'];
+	} else {
+		$timezone="Asia/Tokyo";
+	}
 	date_default_timezone_set($timezone);
 	print_r(json_encode(getdate()));
 ?>	
